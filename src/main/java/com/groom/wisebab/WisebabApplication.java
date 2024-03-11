9663da1918bfb2021146cebb1f411f8b98e4c609
namkikim0718 <113903598+namkikim0718@users.noreply.github.com>
2024-03-11 09:30:37 +0900

    [Chore] 백엔드 프로젝트 생성 및 초기 세팅 - #1

---
+++ src/main/java/com/groom/wisebab/WisebabApplication.java
@@ -0,0 +1,13 @@
+package com.groom.wisebab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WisebabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WisebabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/groom/wisebab/WisebabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groom.wisebab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WisebabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
