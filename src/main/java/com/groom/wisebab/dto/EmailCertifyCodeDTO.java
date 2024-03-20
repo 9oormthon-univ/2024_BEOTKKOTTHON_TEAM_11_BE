9132757dcf41b5c4b5e48017a5e51b4519ac43d9
namkikim0718 <113903598+namkikim0718@users.noreply.github.com>
2024-03-21 02:13:22 +0900

    [Feat] Email verification 구현 - #10

---
+++ src/main/java/com/groom/wisebab/dto/EmailCertifyCodeDTO.java
@@ -0,0 +1,15 @@
+package com.groom.wisebab.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class EmailCertifyCodeDTO {
+
+    private String email;
+
+    private String univName;
+
+    private int code;
+}
