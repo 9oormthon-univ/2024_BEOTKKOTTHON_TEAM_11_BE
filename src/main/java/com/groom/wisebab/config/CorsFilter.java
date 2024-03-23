6fa766ef15f274d2f4be9dbad271f6de2147c4ae
namkikim0718 <113903598+namkikim0718@users.noreply.github.com>
2024-03-24 04:42:21 +0900

    [Fix] 필터 헤더 수정 - #41

--- src/main/java/com/groom/wisebab/config/CorsFilter.java
+++ src/main/java/com/groom/wisebab/config/CorsFilter.java
@@ -29,6 +29,8 @@ public class CorsFilter implements Filter {
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers",
                 "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+        response.setHeader("Access-Control-Expose-Headers", "Authorization");
+
 
         if("OPTIONS".equalsIgnoreCase(request.getMethod())) {
             response.setStatus(HttpServletResponse.SC_OK);
